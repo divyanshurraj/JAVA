c6782f6eaa3e2bdb91a2048b2cd878e6ff9fea86
Divyanshu Raj <101365177+divyanshurraj@users.noreply.github.com>
2022-09-23 08:11:24 +0530

    Create Checking the number is even or odd.java

---
+++ Checking the number is even or odd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class A {
+    public static void main(String[] args) {
+        int a ;
+        Scanner ba=new Scanner(System.in);
+        System.out.println("Enter the number :");
+        a=ba.nextInt();
+        System.out.println("Enter the number :");
+        if (a%2==0)
+        System.out.println("The number is even.");
+        else
+        System.out.println("The number is odd.");
+    }
+}
