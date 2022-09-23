c05265a5d43462dec07c7798d3c457e0e38ac950
Divyanshu Raj <101365177+divyanshurraj@users.noreply.github.com>
2022-09-23 08:43:37 +0530

    Create Division.java

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class A {
+    public static void main(String[] args) {
+        float a,b,c;
+       Scanner ba=new Scanner(System.in);
+       System.out.println("Enter the first number:");
+       a=ba.nextFloat();
+       System.out.println("Enter the second number:");
+       b=ba.nextFloat();
+       c=a/b;
+       System.out.println("The Division of the number is: "+c);
+    }
+}
